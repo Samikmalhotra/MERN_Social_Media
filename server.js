909ddd7e0368f05122fdf89c1d77b2cc2fbd0250
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-14 12:04:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/',(req,res)=>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(ORT, ()=>{
-    console.log('Server is up on port ${PORT}') 
+app.listen(PORT, ()=>{
+    console.log('Server is up on port '+ PORT) 
 });
