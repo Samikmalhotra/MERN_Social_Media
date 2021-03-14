31b00aca4ab8ec962b48e01ecf5a439800c8140d
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-14 12:01:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.send("API is running")
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(ORT, ()=>{
-    console.log('Server is up on port ${PORT}')
+    console.log('Server is up on port ${PORT}') 
 });
