31f64086694c54a1554dfd78f7633d82528089b7
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-16 14:02:22 +0530

    update users.js

--- models/User.js
+++ models/User.js
@@ -22,4 +22,4 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model('user', UserSchema); 
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,11 +8,12 @@ const User = require('../../models/User')
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post('/',[
-    check('name', 'Name is required').not().isEmpty(),
-    check('email',"Please include a valid email address").isEmail(),
-    check('password',"Please enter a password of atleast 6 characters").isLength({min: 6})
-],async(req,res)=>{
+router.post('/',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email',"Please include a valid email address").isEmail(),
+        check('password',"Please enter a password of atleast 6 characters").isLength({min: 6})
+    ],async(req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
@@ -22,11 +23,10 @@ router.post('/',[
     try{
         let user = User.findOne({email});
 
+        // See if user exists
         if(user){
             res.status(400).json({errors: [{msg:"User already exists"}]});
         }
-
-        // See if user exists
 
         // Get users gravatar
 
