0a70708f14f55b545b6fd39255ee86c7711dec2e
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-16 13:42:57 +0530

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult} = require('express-validator/check');
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post('/',(req,res)=>{
+router.post('/',[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email',"Please include a valid email address").isEmail(),
+    check('password',"Please enter a password of atleast 6 characters").isLength({min: 6})
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
     console.log(req.body);
     res.send('User route')
 });
