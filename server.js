93dc086146fda9be521672d275fa94c714935660
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-16 11:30:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/',(req,res)=>{
 
 // Define routes
 app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
+
 
 const PORT = process.env.PORT || 5000
 
