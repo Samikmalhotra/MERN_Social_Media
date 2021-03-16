b553e5f7f579a439ccb6dcc14bd32d94afa3f032
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-16 11:22:47 +0530

    Update users.js

---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET api/users
+// @desc TEST route
+// @access Public
+router.get('/',(req,res)=>res.send('User route'));
