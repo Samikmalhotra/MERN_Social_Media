ace33d7166fea13000a415907a865afbfde5000b
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-16 11:13:06 +0530

    fixed bugs

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ const db = config.get('mongoURI');
 
 const connectDB = async() =>{
     try{
-        await mongoose.connect(db);
+        await mongoose.connect(db,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         
         console.log('MongoDB connected');
     }catch(e){
