2fa6ede21b03a1b0f9276dba13bd22f653834188
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-16 13:52:47 +0530

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,22 +11,27 @@ router.post('/',[
     check('name', 'Name is required').not().isEmpty(),
     check('email',"Please include a valid email address").isEmail(),
     check('password',"Please enter a password of atleast 6 characters").isLength({min: 6})
-],(req,res)=>{
+],async(req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
+    const {name,email,password} = req.body;
+
+    try{
+        // See if user exists
+
+        // Get users gravatar
+
+        // Encrypt password
+
+        // Return jsonwebtoken
+
+    }catch(e){
+
+    }
     res.send('User route')
 });
 
-const {name,email,password} = req.body
-
-// See if user exists
-
-// Get users gravatar
-
-// Encrypt password
-
-// Return jsonwebtoken
 
 module.exports = router;
