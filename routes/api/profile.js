e71e7637af26a9d690cba3a1d9147d5ca0c531ef
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-17 14:16:55 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    TEST route
-// @access     Public
-router.get('/',(req,res)=>res.send('Profile route'));
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/',async(req,res)=>{
+    try{
+
+    }catch(e){
+        console.error(e.message)
+        res.status(500).send('Server Error!')
+    }
+});
 
 module.exports = router;
