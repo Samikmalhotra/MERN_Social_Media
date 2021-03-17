054927c7884c4db4874cb51f53ffe814b5c47a6f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-17 12:30:42 +0530

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -46,6 +46,11 @@ router.post('/',
                return res.status(400).json({errors: [{msg:"Invalid Credentials"}]});
             }
 
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch){
+                return res.status(400).json({errors: [{msg:"Invalid Credentials"}]});
+            }
+
             // Return jsonwebtoken
             const payload = {
                 user:{
