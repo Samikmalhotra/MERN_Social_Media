e22b657a4fc579d3a89982703f3d2631431ff21f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-17 14:19:26 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
+const Profile = require ('../../models/Profile');
+const User = require('../../models/User');
+
 // @route   GET api/profile/me
 // @desc    Get current users profile
 // @access  Private
 router.get('/',async(req,res)=>{
     try{
-
+        const profile = await Profile.findOne({user: req.user.id});
     }catch(e){
         console.error(e.message)
         res.status(500).send('Server Error!')
