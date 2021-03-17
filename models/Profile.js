9171a891d7bf7b05f750c5394b1ab7559b518372
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-17 16:50:09 +0530

    fixed bug

--- models/Profile.js
+++ models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user '
+        ref: 'user'
     },
     website:{
         type:String
