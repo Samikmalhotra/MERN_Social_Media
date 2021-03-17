feb2cd947c6b41cfa4c8bb83621dedfa20d299f9
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-17 17:42:06 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {check,validationResult} = require('express-validator')
 
 const Profile = require ('../../models/Profile');
 const User = require('../../models/User');
@@ -22,5 +23,70 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills are required').not().isEmpty()
+]], 
+async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const{
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin,
+    } = req.body
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+    
+    if(skills){
+        profileFields.skills = skills.split(',').map(skill=> skill.trim())
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if(youtube) profileFields.social.youtube = youtube;
+    if(twitter) profileFields.social.twitter = twitter;
+    if(facebook) profileFields.social.facebook = facebook;
+    if(linkedin) profileFields.social.linkedin = linkedin;
+    if(instagram) profileFields.social.instagram = instagram;
+
+    try{
+        let profile = await Profile.findOne({user: req.user.id});
+
+        if(profile){
+            // Update
+            profile = await Profile.findOneAndUpdate({user: req.user.id},{$set:profileFields},{new: true});
+        }
+    }catch(e){
+        console.error(e.message);
+        res.status(500).send('Server Error!');
+    }
+    console.log(profileFields.skills);
+    res.send('Hello');
+})
+
+
 
 module.exports = router;
