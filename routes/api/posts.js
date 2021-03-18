c068a19208b24d57c1bd07f7372a1ca5fcba60dd
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-19 00:22:15 +0530

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,4 +52,27 @@ router.get('/', auth, async(req,res)=>{
     }
 })
 
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private 
+router.get('/:id', auth, async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({msg:'Post not found'})
+        }
+
+        res.json(post);
+    } catch (e) {
+        console.error(e.message);
+
+        if(e.kind === 'ObjectId'){
+            return res.status(404).json({msg:'Post not found'})
+        }
+        
+        res.status(500).send('Server Error!');
+    }
+})
 module.exports = router;
