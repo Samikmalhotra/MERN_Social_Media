ba02c119004bc836b8c6a14ef085a7587b30c87d
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-19 00:32:51 +0530

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -75,4 +75,31 @@ router.get('/:id', auth, async(req,res)=>{
         res.status(500).send('Server Error!');
     }
 })
+
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post 
+// @access  Private 
+router.delete('/:id', auth, async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check User
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg:'User not authorised'});
+        }
+
+        await post.remove();
+
+        res.json({msg:'Post removed'})
+        
+    } catch (e) {
+        console.error(e.message);
+        if(e.kind === 'ObjectId'){
+            return res.status(404).json({msg:'Post not found'})
+        }
+        res.status(500).send('Server Error!')
+    }
+})
+
 module.exports = router;
