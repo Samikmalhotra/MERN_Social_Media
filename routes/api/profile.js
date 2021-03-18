be67b167b4240044d7476dd955c9041af5a4bad5
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-18 13:26:56 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,16 +154,17 @@ router.delete('/',auth, async(req,res)=>{
 // @route    PUT api/profile/experience
 // @desc     Add profile experience
 // @access   Private
-router.put('/experience', [auth[
+
+router.put('/experience', [auth, [
     check('title', 'Title is required').not().isEmpty(),
     check('company', 'Company is required').not().isEmpty(),
-    check('from', 'From date is required').not().isEmpty()
+    check('from', 'From is required').not().isEmpty(),
 ]], 
 async(req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    } 
+        return res.status(400).json({errors: errors.array()})
+    }
 
     const {
         title,
