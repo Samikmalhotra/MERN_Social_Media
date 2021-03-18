83a23c3aaa1c1178b08991206f073a20f3e40206
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-18 23:15:32 +0530

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const {check,validationResult} = require('express-validator')
+const auth = require('../../middleware/auth')
+
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User =require('../../models/User');-
+
 
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private 
 router.post('/', [auth, [
-
+    check('text', 'Text is required').not().isEmpty()
 ]],
 async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
 
+    try {
+        const user = await  User.findById(req.user.id).select('-password');
+
+        const newPost = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        }
+
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error!');
+    }
 });
 
 module.exports = router;
