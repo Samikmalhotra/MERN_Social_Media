32cb079fba97052026de8b3f964c878f453c9e53
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-18 13:59:55 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,10 +211,12 @@ router.delete('/experience/:exp_id', auth, async(req,res)=>{
 
         // Get remove index
         const removeIndex = profile.experience.map(item=>item.id).indexOf(req.params.exp_id);
-        
+
         profile.experience.splice(removeIndex,1);
 
         await profile.save();
+
+        res.json(profile)
     } catch (e) {
         console.error(e.message);
         res.status(500).send('Server Error!');
