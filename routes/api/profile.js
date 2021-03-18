c8b170c8864d836a2282b89ca73e245822fd22b3
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-18 11:00:00 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -150,4 +150,53 @@ router.delete('/',auth, async(req,res)=>{
         res.status(500).send('Server Error!')
     }
 })
+
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put('/experience', [auth[
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], 
+async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    } 
+
+    const {
+        title,
+        company,
+        current,
+        location,
+        from,
+        to,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        current,
+        location,
+        from,
+        to,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile)
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error!');
+    }
+})
+
 module.exports = router;
