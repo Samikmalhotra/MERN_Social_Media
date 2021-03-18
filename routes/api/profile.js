0d0fbc204bb23ab94314ec3f8444586ebc0b68de
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-18 14:26:57 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const {check,validationResult} = require('express-validator')
 
@@ -293,4 +295,22 @@ router.delete('/education/:edu_id', auth, async(req,res)=>{
         res.status(500).send('Server Error!');
     }
 } )
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async(req,res)=>{
+    try {
+        const options = {
+            uri:'https://api.github.com/users/'+req.params.username+'/repos?per_page=5&sort=created:asc&client_id='
+            +config.get('githubClientID')+'&client_secret'+config.get('githubSecret'),
+            method:'GET',
+            headers:{'user-agent': 'node.js'}
+        }
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error!');
+    }
+})
+
 module.exports = router;
