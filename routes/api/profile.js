1860f1c60e23f104b9e81ebec01dcd03546b4009
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-18 15:32:03 +0530

    fixed bugs

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -301,20 +301,21 @@ router.delete('/education/:edu_id', auth, async(req,res)=>{
 // @access   Public
 router.get('/github/:username', async(req,res)=>{
     try {
-        const uri = encodeURI(
-            `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
-          );
-        const headers = {
-            'user-agent': 'node.js',
-            Authorization: `token ${config.get('githubToken')}`
-          };
-
+        const options = {
+            uri:'https://api.github.com/users/'+req.params.username+'/repos?per_page=5&sort=created:asc&client_id='
+            +config.get('githubClientId')+'&client_secret'+config.get('githubSecret'),
+            method:'GET',
+            headers:{'user-agent': 'node.js'}
+        }
+        
         request(options ,(error,response,body)=>{
             if(error) console.error(error);
 
-            if(response.stausCode !== 200) res.status(404).json({msg:'No github profile found'});
+            if(response.statusCode !== 200){
+                return res.status(404).json({msg:'No github profile found'});
+            } 
 
-            res.send('hello').json(JSON.parse(body));
+            res.json(JSON.parse(body));
         })
     } catch (e) {
         console.error(e.message);
