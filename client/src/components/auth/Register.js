8ad93a2f9007c3d9267cc86a8e630c85ca86b76b
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-20 00:12:06 +0530

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        nsme:'',
+        email:'',
+        password:'',
+        password2:''
+    });
+
+    const { name,email,password,password2 } = formData;
+
     return (
         <Fragment>
            <h1 className="large text-primary">Sign Up</h1>
