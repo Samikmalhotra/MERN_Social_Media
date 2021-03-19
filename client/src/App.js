6b3dc97e4d3358c7182fb8d33ad29354e3966d65
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-19 23:50:57 +0530

    creating auth react

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
 
 const App = () => {
     return(
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path = '/' component = {Landing} />
+          <Route exact path="/" component={Landing} />     
         </Fragment>
       </Router>
     );
