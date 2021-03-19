cfd4db94d522a6079e1e293f9cce3366ea8f24e2
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-19 11:53:37 +0530

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -106,4 +106,29 @@ router.delete('/:id', auth, async(req,res)=>{
     }
 })
 
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post 
+// @access  Private 
+router.put('/like/:id', auth, async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if post has already been liked
+        if(post.likes.filter(like=>like.user.toString()===req.user.id).length>0){
+            res.status(400).json({msg:'Post already liked'});
+        }
+
+        post.likes.unshift({user:req.user.id});
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error!')
+
+    }
+})
+
 module.exports = router;
