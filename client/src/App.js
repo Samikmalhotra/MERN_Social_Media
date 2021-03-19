b4f5d13f1e21807eaf02cef066b4fff7ba09d222
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-19 23:44:15 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing'
 
 const App = () => {
     return(
-      <Fragment>
-        <Navbar />
-        <Landing />
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path = '/' component = {Landing} />
+        </Fragment>
+      </Router>
     );
 }
 
