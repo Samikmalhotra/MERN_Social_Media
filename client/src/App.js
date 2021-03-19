9b7f100090faeaa8158f8b4fbba1261340f5b66d
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-19 19:38:57 +0530

    added Navbar & Landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { Fragment } from 'react';
 import './App.css';
 import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing'
 
 const App = () => {
     return(
       <Fragment>
-        <h1>App</h1>
+        <Navbar />
+        <Landing />
       </Fragment>
     );
 }
