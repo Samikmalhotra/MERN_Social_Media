1261fc1f85202581fa52ec0cc51cc6ba08a19a77
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-21 13:52:41 +0530

    updated store.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,29 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () => {
     return(
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path="/" component={Landing} />   
-          <section className = 'container'>
-            <Switch>
-              <Route exact path = '/register' component={Register} />
-              <Route exact path = '/login' component={Login} />
-            </Switch>
-          </section>  
-        </Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <Route exact path="/" component={Landing} />   
+            <section className = 'container'>
+              <Switch>
+                <Route exact path = '/register' component={Register} />
+                <Route exact path = '/login' component={Login} />
+              </Switch>
+            </section>  
+          </Fragment>
+        </Router>
+      </Provider> 
     );
 }
 
