0f570fb672ebea1ad4cb78b6b77397a09d51c17f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-22 19:55:10 +0530

    fixed errors

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,8 +1,9 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
-import uuid from 'uuid';
+import {v4} from 'uuid';
 
 export const setAlert = (msg, alertType) => dispatch => {
-    const id = uuid.v4();
+    const uuid = v4();
+    const id = uuid;;
     dispatch({
         type: SET_ALERT,
         payload: {msg, alertType, id}
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react'
 // import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert'
 
 
 
-const Register = () => {
+const Register = (props) => {
     const [formData, setFormData] = useState({
         name:'',
         email:'',
@@ -19,7 +21,7 @@ const Register = () => {
     const onSubmit = async(e) => {
         e.preventDefault();
         if(password !== password2){
-            console.log('Passwords do not match');
+            props.setAlert('Passwords do not match', 'danger');
         } else {
         //     const newUser = {
         //         name,
@@ -88,4 +90,4 @@ const Register = () => {
     )
 }
   
-export default Register;
+export default connect(null, {setAlert})(Register);
