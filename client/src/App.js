3d43ddde722e0803e0cf2b95845e5b603d14a293
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-22 20:42:05 +0530

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 // Redux
@@ -18,6 +19,7 @@ const App = () => {
             <Navbar />
             <Route exact path="/" component={Landing} />   
             <section className = 'container'>
+              <Alert />
               <Switch>
                 <Route exact path = '/register' component={Register} />
                 <Route exact path = '/login' component={Login} />
