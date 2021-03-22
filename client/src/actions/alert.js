b18b030fe4d9ea4478af3c818c7ca84eb6b5207f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-22 20:48:06 +0530

    update alert.js

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -8,4 +8,9 @@ export const setAlert = (msg, alertType) => dispatch => {
         type: SET_ALERT,
         payload: {msg, alertType, id}
     });
+
+    setTimeout(()=>dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+    }), 5000);
 }
