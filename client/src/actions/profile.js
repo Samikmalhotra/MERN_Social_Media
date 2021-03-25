3927a2ca0a814bda646035066a79671a3a11a047
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-25 23:13:26 +0530

    added profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -8,18 +8,19 @@ import{
 
 // Get current users profile
 
-export const getCurrentProfile = () => async dispatch => {
+export const getCurrentProfile = () => async (dispatch) => {
     try {
-        const res = await axios.get('api/profile/me');
-
-        dispatch({
-            type: GET_PROFILE,
-            payload: res.data 
-        });
-    } catch (e) {
-        dispatch({
-            type: PROFILE_ERROR,
-            payload: {msg: e.response.statusText, status: e.response.status}
-        })
+      const res = await axios.get('/profile/me');
+  
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
     }
-}
+  };
+  
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile'
 
-const Dashboard = props => {
+const Dashboard = ({
+    getCurrentProfile,
+    auth,
+    profile
+  }) => {
+    useEffect(() => {
+      getCurrentProfile();
+    }, []);
+  
     return (
         <div>
             Dashboard
@@ -10,7 +20,14 @@ const Dashboard = props => {
 }
 
 Dashboard.propTypes = {
-
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
 }
 
-export default Dashboard
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    profile: state.profile
+})
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard)
