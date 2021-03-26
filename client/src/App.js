dba8a6637a650983877e4a2cd161337adfaf9f98
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-27 00:01:42 +0530

    aded profiles by id

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,8 @@ import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
-import Profiles from './components/profile/Profiles'
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile'
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
@@ -44,6 +45,7 @@ const App = () => {
                 <Route exact path = '/register' component={Register} />
                 <Route exact path = '/login' component={Login} />
                 <Route exact path = '/profiles' component={Profiles} />
+                <Route exact path = '/profile/:id' component={Profile} />
                 <PrivateRoute exact path = '/dashboard' component={Dashboard} />
                 <PrivateRoute exact path = '/create-profile' component={CreateProfile} />
                 <PrivateRoute exact path = '/edit-profile' component={EditProfile} />
