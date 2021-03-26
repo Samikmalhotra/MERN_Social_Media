f6a4e4bc09bdec50a2bec75037dbef9d7a46a122
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-26 13:58:10 +0530

    added updateProfile action and reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,8 @@ import {setAlert} from './alert';
 
 import{
     GET_PROFILE,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    UPDATE_PROFILE
 } from './types';
 
 // Get current users profile
@@ -49,6 +50,74 @@ export const getCurrentProfile = () => async dispatch => {
       if(!edit){
         history.push('/dashboard')
       }
+    } catch (e) {
+      const errors = e.response.data.errors;
+
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: e.response.statusText, status: e.response.status }
+      });
+    }
+  }
+
+  // Add experience
+  export const addExperience = (formData, history) => async dispatch => {
+    try {
+      const config = {
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }
+
+      const res = await axios.put('/api/profile/experience',formData, config);
+
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+
+      dispatch(setAlert('Experience added', 'success'));
+
+      history.push('/dashboard');
+  
+    } catch (e) {
+      const errors = e.response.data.errors;
+
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: e.response.statusText, status: e.response.status }
+      });
+    }
+  }
+
+   // Add education
+   export const addEducation = (formData, history) => async dispatch => {
+    try {
+      const config = {
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }
+
+      const res = await axios.put('/api/profile/education',formData, config);
+
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+
+      dispatch(setAlert('Education added', 'success'));
+
+      history.push('/dashboard');
+  
     } catch (e) {
       const errors = e.response.data.errors;
 
