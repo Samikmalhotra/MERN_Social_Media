9aee515fddc55bf771e589b467fdb6c848c42cc6
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-26 17:04:43 +0530

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const {check,validationResult} = require('express-validator')
 
 const Profile = require ('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const { reset } = require('nodemon');
 
 // @route    GET api/profile/me
@@ -141,7 +142,8 @@ router.get('/user/:user_id', async(req,res)=>{
 
 router.delete('/',auth, async(req,res)=>{
     try {
-        // @todo - Remove user posts 
+        // Remove user posts 
+        await Post.deleteMany({user: req.user.id});
         // Remove profile
         await Profile.findOneAndRemove({user: req.user.id});
         // Remove user
