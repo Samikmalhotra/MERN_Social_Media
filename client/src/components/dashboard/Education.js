5a9a14870bc398a051d790c212e6695024754b00
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-26 16:53:55 +0530

    Update Education.js

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,9 +2,9 @@ import React,{Fragment} from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
+import {deleteEducation} from '../../actions/profile'
 
-
-const Education = ({education}) => {
+const Education = ({education, deleteEducation}) => {
     const educations = education.map(edu => (
         <tr key={edu._id}>
             <td>{edu.school}</td>
@@ -15,7 +15,7 @@ const Education = ({education}) => {
                 }
             </td>
             <td>
-                <button className='btn btn-danger'>Delete</button>
+                <button onClick={()=>{deleteEducation(edu._id)}} className='btn btn-danger'>Delete</button>
             </td>
         </tr>
     ));
@@ -41,7 +41,8 @@ const Education = ({education}) => {
 }
 
 Education.propTypes = {
-    education: PropTypes.array.isRequired
+    education: PropTypes.array.isRequired,
+    deleteEducation: PropTypes.func.isRequired,
 }
 
-export default Education;
+export default connect(null, {deleteEducation})(Education);
